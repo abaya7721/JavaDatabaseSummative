1d38f424073f3f5b0c1ee2c91da49e82ad4ed956
abaya7721 <62572548+abaya7721@users.noreply.github.com>
2025-02-14 10:38:17 -0700

    OrderRepositoryTest edits

--- src/main/java/com/assessment/bistro/model/OrderItem.java
+++ src/main/java/com/assessment/bistro/model/OrderItem.java
@@ -1,39 +1,26 @@
 package com.assessment.bistro.model;
 
-import jakarta.persistence.*;
-import org.springframework.data.annotation.Id;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Collection;
 
 @Entity
-public class Order {
+public class OrderItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int orderId;
     private int serverId;
-    @Temporal(TemporalType.DATE)
     private LocalDateTime orderDate;
     private BigDecimal subTotal;
     private BigDecimal tax;
+
     private BigDecimal tip;
-    private BigDecimal total;
 
-    public Order(){
-
-    }
-
-    public Order(int orderId, int serverId, LocalDateTime orderDate, BigDecimal subTotal, BigDecimal tax, BigDecimal tip, BigDecimal total) {
-        this.orderId = orderId;
-        this.serverId = serverId;
-        this.orderDate = orderDate;
-        this.subTotal = subTotal;
-        this.tax = tax;
-        this.tip = tip;
-        this.total = total;
-    }
+    // Getters and setters
 
     public int getOrderId() {
         return orderId;
@@ -82,29 +69,5 @@ public class Order {
     public void setTip(BigDecimal tip) {
         this.tip = tip;
     }
-
-    public BigDecimal getTotal() {
-        return total;
-    }
-
-    public void setTotal(BigDecimal total) {
-        this.total = total;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "orderId=" + orderId +
-                ", serverId=" + serverId +
-                ", orderDate=" + orderDate +
-                ", subTotal=" + subTotal +
-                ", tax=" + tax +
-                ", tip=" + tip +
-                ", total=" + total +
-                '}';
-    }
-
-    //    public Collection<Object> getItems() {
-//
-//    }
 }
+
--- src/test/java/com/assessment/bistro/repository/OrderRepositoryTest.java
+++ src/test/java/com/assessment/bistro/repository/OrderRepositoryTest.java
@@ -1,51 +1,94 @@
-//package com.assessment.bistro.repository;
-//
-//import com.assessment.bistro.model.Order;
-//import com.assessment.bistro.model.OrderItem;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest
-//class OrderRepositoryTest {
-//    @Autowired
-//    private OrderRepository repo;
-//
-//    @BeforeEach
-//    void setUp() {
-//
-//    }
-//
-//    @Test
-//    public void testGetOrderById() {
-//
-//
-//    }
-//
-//    @Test
-//    public void testGetAllOrders() {
-//
-//    }
-//
-//    @Test
-//    public void testAddOrder() {
-//
-//    }
-//
-//    @Test
-//    public void testDeleteOrder() {
-//
-//    }
-//
-//    @Test
-//    public void testUpdateOrder() {
-//
-//    }
-//
-//
+package com.assessment.bistro.repository;
+
+import com.assessment.bistro.model.Order;
+import com.assessment.bistro.model.OrderItem;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class OrderRepositoryTest {
+
+    @Autowired
+    private OrderRepository repo;
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    public void testGetOrderById() {
+
+        Order orderTestGetById = new Order(4, 14,
+                LocalDateTime.of(2021, 11, 15, 0, 0),
+                BigDecimal.valueOf(73.00),
+                BigDecimal.valueOf(4.20), BigDecimal.valueOf(10.95), BigDecimal.valueOf(88.15));
+
+        Order orderIdFour = repo.getReferenceById(4);
+
+        assertEquals(orderIdFour.toString(), orderTestGetById.toString());
+    }
+
+    @Test
+    public void testGetAllOrders() {
+        int orderTotal = 365;
+
+        int allOrdersTest = repo.findAll().size();
+
+        assertEquals(orderTotal, allOrdersTest);
+    }
+
+    @Test
+    public void testAddOrder() {
+        Order newOrder = new Order(366, 14,
+                LocalDateTime.of(2025, 1, 15, 0, 0),
+                BigDecimal.valueOf(73.00),
+                BigDecimal.valueOf(4.20), BigDecimal.valueOf(10.95), BigDecimal.valueOf(88.15));
+
+        Order addOrder = new Order(366, 14,
+                LocalDateTime.of(2025, 1, 15, 0, 0),
+                BigDecimal.valueOf(73.00),
+                BigDecimal.valueOf(4.20), BigDecimal.valueOf(10.95), BigDecimal.valueOf(88.15));
+
+        Order addedOrder = repo.save(addOrder);
+
+        assertEquals(newOrder.toString(), addedOrder.toString());
+        
+
+    }
+
+    @Test
+    public void testUpdateOrder() {
+        Order updateOrder = new Order(366, 14,
+                LocalDateTime.of(2025, 1, 20, 0, 0),
+                BigDecimal.valueOf(73.00),
+                BigDecimal.valueOf(4.20), BigDecimal.valueOf(10.95), BigDecimal.valueOf(100.00));
+
+        Order order = repo.getReferenceById(366);
+        order.setTotal(BigDecimal.valueOf(100.00));
+        order.setOrderDate(LocalDateTime.of(2025, 1, 20, 0, 0));
+        order = repo.save(order);
+
+        assertEquals(updateOrder, order);
+    }
+
+    @Test
+    public void testDeleteOrder() {
+
+        repo.delete(repo.getReferenceById(366));
+
+        assertEquals(365, repo.findAll().size());
+    }
+
+
 //    private void compareOrders(Order expected, Order actual) {
 //        assertEquals(expected.getOrderId(), actual.getOrderId());
 //        assertEquals(expected.getSubTotal(), actual.getSubTotal());
@@ -64,5 +107,5 @@
 //            assertEquals(e.getQuantity(), a.getQuantity());
 //            assertEquals(e.getPrice(), a.getPrice());
 //        }
-//    }
-//}
+
+}
