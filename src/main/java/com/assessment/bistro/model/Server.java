024663b6249d38c412664122db9bf3b2279f3324
abaya7721 <62572548+abaya7721@users.noreply.github.com>
2025-02-14 03:08:32 -0700

    ServerRepository tests completion

---
+++ src/main/java/com/assessment/bistro/model/Server.java
@@ -0,0 +1,84 @@
+package com.assessment.bistro.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Server implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int serverId;
+    private String firstName;
+    private String lastName;
+    @Temporal(TemporalType.DATE)
+    private LocalDate hireDate;
+    @Temporal(TemporalType.DATE)
+    private LocalDate termDate;
+
+    public Server() {
+    }
+
+    public Server(int serverId, String firstName, String lastName, LocalDate hireDate, LocalDate termDate) {
+        this.serverId = serverId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hireDate = hireDate;
+        this.termDate = termDate;
+    }
+
+    public int getServerId() {
+        return serverId;
+    }
+
+    public void setServerId(int serverId) {
+        this.serverId = serverId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public LocalDate getTermDate() {
+        return termDate;
+    }
+
+    public void setTermDate(LocalDate termDate) {
+        this.termDate = termDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Server{" +
+                "serverId=" + serverId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", hireDate=" + hireDate +
+                ", termDate=" + termDate +
+                '}';
+    }
+}
+
--- src/test/java/com/assessment/bistro/repository/ServerRepositoryTest.java
+++ src/test/java/com/assessment/bistro/repository/ServerRepositoryTest.java
@@ -1,38 +1,73 @@
-//package com.assessment.bistro.repository;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class ServerRepositoryTest {
-//
-//    @Autowired
-//    private ServerRepository repo;
-//
-//    @BeforeEach
-//    void setUp() {
-//
-//    }
-//
-//    @Test
-//    void testGetServerById() {
-//
-//    }
-//
-//    @Test
-//    void testGetAllAvailableServers() {
-//
-//    }
-//
-//    @Test
-//    void testUpdateServer() {
-//
-//    }
-//
-//    @Test
-//    void testAddServer() {
-//
-//    }
-//}
+package com.assessment.bistro.repository;
+
+import com.assessment.bistro.model.Server;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.LocalDate;
+import java.util.Optional;
+
+
+@SpringBootTest
+public class ServerRepositoryTest {
+
+    Server expectedRecord1;
+    Server expectedRecord2;
+
+    @Autowired
+    private ServerRepository repo;
+
+    @BeforeEach
+    void setUp() {
+
+        //Server1
+        expectedRecord1 = new Server();
+        expectedRecord1.setServerId(2);
+        expectedRecord1.setFirstName("Zita");
+        expectedRecord1.setLastName("McClintock");
+        expectedRecord1.setHireDate(LocalDate.of(2022, 12, 5));
+    }
+
+    @Test
+    void testGetServerById() {
+        Optional<Server> serverWithIdOne = repo.findById(2);
+        assertEquals(expectedRecord1.toString(), serverWithIdOne.get().toString());
+    }
+
+    @Test
+    void testGetAllAvailableServers() {
+        int servers = repo.findAll().size();
+        assertEquals(22, servers);
+    }
+
+    @Test
+    void testUpdateServer() {
+        Optional<Server> server = repo.findById(1);
+        server.get().setFirstName("UpdatedFirstName");
+        server.get().setLastName("UpdatedLastName");
+
+        repo.save(server.get());
+
+        Optional<Server> updatedServer = repo.findById(1);
+        assertEquals("UpdatedFirstName", updatedServer.get().getFirstName());
+        assertEquals("UpdatedLastName", updatedServer.get().getLastName());
+    }
+
+    @Test
+    void testAddServer() {
+
+            Server newServer = new Server();
+            newServer.setFirstName("New");
+            newServer.setLastName("Person");
+            newServer.setHireDate(LocalDate.of(2022, 12, 13));
+            newServer.setTermDate(null);
+
+            Server savedServer = repo.save(newServer);
+
+            assertEquals("New", savedServer.getFirstName());
+            assertEquals("Person", savedServer.getLastName());
+            assertEquals(LocalDate.of(2022, 2, 13), savedServer.getHireDate());
+        }
+}
